e244cdca79be361e8ad83b3af1a3fb3ad495c9e1
adrianmulyawan <adrianmulyawan666@gmail.com>
2024-02-21 13:58:41 +0700

    Add relation : User and Service

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'icon',
+        'name',
+        'description'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -68,4 +68,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Banner::class);
     }
+
+    public function services()
+    {
+        return $this->hasMany(Service::class);
+    }
 }
